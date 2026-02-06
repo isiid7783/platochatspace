0fa17a4bf422d68652bf2f679370f968d16cfba8
isiid7783 <llcterraio@gmail.com>
2026-02-06 21:31:59 +0900

    Update libp2p.js

--- src/libp2p.js
+++ src/libp2p.js
@@ -3,7 +3,6 @@ import { webRTC } from "@libp2p/webrtc"
 import { webSockets } from "@libp2p/websockets"
 import { noise } from "@chainsafe/libp2p-noise"
 import { mplex } from "@libp2p/mplex"
-import { kadDHT } from "@chainsafe/libp2p-kad-dht"
 
 export async function createNode() {
   const node = await createLibp2p({
@@ -18,3 +17,4 @@ export async function createNode() {
   await node.start()
   return node
 }
+
